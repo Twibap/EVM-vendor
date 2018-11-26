c2a0f491b6fe8a60d2fa5ae50205ac3081c8cd96
TK Yu <tky476@gmail.com>
2018-11-26 20:23:17 +0900

    apply promise to payment stage

--- routes/order.js
+++ routes/order.js
@@ -81,10 +81,9 @@ router.post('/askN', (request, response)=>{
 // 결제정보 검증단계
 // Client가 전송한 결제 정보가 정상이면 Ether를 송금한다.
 router.post('/payment', (request, response)=>{
-	// 결제 검증 
 	var bill = transaction.mkBill( request.body );
-	console.log( JSON.stringify( bill ) );
 
+	// 결제 검증 
 	BootpayRest.getAccessToken().then(function (response) {
 		// Access Token을 발급 받았을 때
 		if (response.status === 200 && response.data.token !== undefined) {
@@ -92,43 +91,45 @@ router.post('/payment', (request, response)=>{
 			return BootpayRest.verify( bill.receipt_id );
 		}
 	}).then(function (_response) {
-		//console.log(_response);
-
 		// 검증 결과를 제대로 가져왔을 때
 		if (_response.status === 200) {
 			// 원래 주문했던 금액이 일치하는가?
 			// 그리고 결제 상태가 완료 상태인가?
 			if (_response.data.price == bill.price && _response.data.status === 1) {
-				// 인증 정보 저장 및 order_id 업데이트
+				console.log( colors.info("Payment Ok") );
+
+				// 인증 정보 저장
 				bill.unit = _response.data.unit;
-				bill.save((error)=>{if(error!=null) console.error; return;});
-
-				transaction
-					.Order.updateOne(
-						{_id: bill.order_id}, 
-						{bill_id: bill._id},
-						(error, resultFromDB)=>{
-							if (error) return handleError(error);
-							console.log( colors.info("Payment Ok") );
-							console.log( colors.info( resultFromDB ) );
-
-							transaction
-								.Order.findById(bill.order_id).exec(
-								(error, order)=>{
-									if (error) return handleError(error);
-									console.log( colors.info( order ) );
-								}
-							);
-						}
-					);
-
-				// TODO Ether Vending Contract에 전송 Tx 발행
-				
-				// TODO Tx Hash 반환
-				response.end( "result : "+ true);
-
+				return bill.save();
 			}
 		}
+	}).then((savedBill)=>{
+
+		// 결제 id를 Order에 저장
+		return transaction.Order.updateOne(
+			{_id: savedBill.order_id},
+			{bill_id: savedBill._id});
+
+	}).then((updateResult)=>{
+		if( updateResult.ok )		// { n: 1, nModified: 1, ok: 1 }
+			console.log( colors.info( "Payment Saved and Order update" ) );	
+		else
+			throw Error("Payment data save is Failed");
+
+		return transaction.Order.findById(bill.order_id);
+	}).then((updatedOrder)=>{
+		console.log( colors.info( updatedOrder ) );	
+
+		// TODO Ether Vending Contract에 전송 Tx 발행
+		return new Promise((resolve, reject)=>{
+			resolve();
+		});
+	}).then(()=>{
+		// TODO Tx Hash 반환
+		response.end( "result : "+ true);
+	}).catch((error)=>{
+		response.status(500);
+		response.end( error.toString() );
 	});
 });
 
