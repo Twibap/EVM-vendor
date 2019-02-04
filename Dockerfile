d4599f28ef9b0a9a2ee457e13ea0278772c911a0
TK Yu <tky476@gmail.com>
2019-02-04 19:21:10 +0900

    DB 주소 입력 가능

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,9 @@ RUN npm install
 # 앱 복사
 COPY . .
 
+# 호스트와 연결할 포트 설정
 EXPOSE 8080
-CMD ["npm","start"]
+
+# 앱 실행
+# docker run -e DATABASE_URL=127.0.0.1 -e DATABASE_PORT=27017 --name evm-vendor -p 3000:3000 -v ~/release:/usr/src/release -d twibap/evm-vendor
+CMD node app.js -d $DATABASE_URL -p $DATABASE_PORT
--- app_modules/database.js
+++ app_modules/database.js
@@ -7,13 +7,40 @@ const data = require('../confidential.js');
 /* ====== DB Setting ========================= */
 var mongoose = require("mongoose");
 var db = mongoose.connection;
-//var dbUrl = 'mongodb://localhost/';
-var dbUrl = 'mongodb://'+data.DATABASE_URL;
+
+// 실행 매개변수로부터 Database 주소와 Port를 가져온다.
+const command = process.argv;
+
+let database_ip;
+let database_port;
+process.argv.forEach(function (item, index){
+	switch( item ){
+		case '--database':
+		case '-d':
+			database_ip = process.argv[ index +1 ];
+			break;
+		case '--port':
+		case '-p':
+			database_port = process.argv[ index +1 ];
+			break;
+	}
+
+	if( database_ip == null ){
+		database_ip = '127.0.0.1';
+	}
+	
+	if( database_port == null ){
+		database_port = '27017';
+	}
+});
+
+var dbUrl = 'mongodb://'+database_ip+':'+database_port;
 var dbName = '/EVM';
 
 mongoose.Promise = global.Promise;
 db.on('error', console.error);
 db.once('open', ()=>{
-	console.log( colors.info("Database is connected") );
+	console.log( colors.info(dbUrl+" is connected") );
 });
+
 mongoose.connect( dbUrl+dbName, {useNewUrlParser: true} );
